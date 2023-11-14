ff965c9aa486a9ee4d8e576487698de4c5668c8f
HansEscobedo <hans.escobedo@alumnos.ucn.cl>
2023-11-14 09:14:11 -0300

    CRUD terminado y error en web

--- app/src/main/java/com/example/evaluaciondsm/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/evaluaciondsm/ui/dashboard/DashboardFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,12 +17,26 @@ import com.example.evaluaciondsm.databinding.FragmentDashboardBinding;
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private EditText txt_url;
+    private WebView webview;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+        txt_url = binding.editTextText;
+        webview = binding.webView1;
+
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webview.loadUrl(txt_url.getText().toString());
+
+            }
+        });
 
         return root;
     }
--- app/src/main/java/com/example/evaluaciondsm/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/evaluaciondsm/ui/home/HomeFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,17 +15,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.evaluaciondsm.AdminSQLiteOpenHelper;
 import com.example.evaluaciondsm.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private EditText txt_code, txt_description, txt_price;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        txt_code = binding.txtCode;
+        txt_description = binding.txtDescription;
+        txt_price = binding.txtPrice;
 
         binding.btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,24 +70,107 @@ public class HomeFragment extends Fragment {
     }
 
     //Metodo para guardar los productos
-    public void save() {
-
+    public void save(){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this.getActivity(), "administracion", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+        String code = txt_code.getText().toString();
+        String description = txt_description.getText().toString();
+        String price = txt_price.getText().toString();
+        if(!code.isEmpty() && !description.isEmpty() && !price.isEmpty()) {
+            Cursor fila = db.rawQuery("select codigo from articulos where codigo = " + code, null);
+            if(fila.moveToFirst()) {
+                Toast.makeText(this.getActivity(), "Ya existe un artículo con ese código", Toast.LENGTH_SHORT).show();
+                db.close();
+                return;
+            }
+            ContentValues registro = new ContentValues();
+            registro.put("codigo", code);
+            registro.put("descripcion", description);
+            registro.put("precio", price);
+            db.insert("articulos", null, registro);
+            db.close();
+            cleanForm();
+            Toast.makeText(this.getActivity(), "Registro exitoso", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this.getActivity(), "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
+        }
     }
 
     //Metodo para buscar productos
     public void search() {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this.getActivity(), "administracion", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+        String code = txt_code.getText().toString();
+        if(!code.isEmpty()) {
+            Cursor fila = db.rawQuery("select descripcion, precio from articulos where codigo = " + code, null);
+            if(fila.moveToFirst()) {
+                txt_description.setText(fila.getString(0));
+                txt_price.setText(fila.getString(1));
+                db.close();
+            }
+            else{
+                Toast.makeText(this.getActivity(), "No existe el articulo", Toast.LENGTH_SHORT).show();
+                db.close();
+            }
+        }
+        else{
+            Toast.makeText(this.getActivity(), "Debes introducir el codigo del articulo", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
     public void delete() {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this.getActivity(), "administracion", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+        String code = txt_code.getText().toString();
+        if (!code.isEmpty())
+        {
+            int count = db.delete("articulos", "codigo=" + code, null);
+            if(count == 1) {
+                Toast.makeText(this.getActivity(), "Articulo eliminado", Toast.LENGTH_SHORT).show();
+                db.close();
+                cleanForm();
+            }
+            else{
+                Toast.makeText(this.getActivity(), "El articulo no existe", Toast.LENGTH_SHORT).show();
+                db.close();
+            }
+        }
 
     }
 
     public void edit() {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this.getActivity(), "administracion", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+        String code = txt_code.getText().toString();
+        String description = txt_description.getText().toString();
+        String price = txt_price.getText().toString();
+        if(!code.isEmpty() && !description.isEmpty() && !price.isEmpty()) {
+            ContentValues registro = new ContentValues();
+            registro.put("codigo", code);
+            registro.put("descripcion", description);
+            registro.put("precio", price);
+            int count = db.update("articulos", registro, "codigo=" + code, null);
+            if(count == 1) {
+                Toast.makeText(this.getActivity(), "Articulo modificado", Toast.LENGTH_SHORT).show();
+                db.close();
+            }
+            else{
+                Toast.makeText(this.getActivity(), "El articulo no existe", Toast.LENGTH_SHORT).show();
+                db.close();
+            }
+        }
+        else{
+            Toast.makeText(this.getActivity(), "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
     public void cleanForm() {
+        txt_code.setText("");
+        txt_description.setText("");
+        txt_price.setText("");
     }
 
 }
